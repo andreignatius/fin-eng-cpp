6aaa71cf2ede5913d1c18b8bc84e380bfe28fbae
andreignatius <andre.callme@gmail.com>
2024-05-17 20:39:39 +0800

    split bond.h and .cpp files

--- Bond.h
+++ Bond.h
@@ -10,18 +10,9 @@ public:
 
     Bond(Date start, Date end, double _notional, double _price, double _couponRate, string _underlying):
         Trade("BondTrade", start, _underlying), notional(_notional), tradePrice(_price),
-        startDate(start), maturityDate(end), couponRate(_couponRate), underlying(_underlying) {
-        	std::cout << "999create bond with name :" << _underlying << std::endl;
-        }
+        startDate(start), maturityDate(end), couponRate(_couponRate), underlying(_underlying) {}
 
-    inline double Payoff(double marketPrice) const override {
-	    std::cout << "*bondPrice: " << marketPrice << std::endl;
-	    double priceDifference = (marketPrice - tradePrice) / 100;  // Adjusted for bond pricing
-	    double payoff = priceDifference * notional;
-	    std::cout << "notional :" << notional << " tradePrice: " << tradePrice << " marketPrice: " << marketPrice << std::endl;
-	    std::cout << "@compute payoff:" << payoff << std::endl;
-	    return payoff;
-	}
+    double Payoff(double marketPrice) const override;
 
 private:
     double notional;
