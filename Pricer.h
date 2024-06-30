c6acc8ced094bcd7762599e234fa7dc69537939c
Joseph Adhika <jt.adhika.2023@mqf.smu.edu.sg>
2024-06-30 23:02:33 +0800

    formatting

--- Pricer.h
+++ Pricer.h
@@ -1,86 +1,86 @@
 #ifndef _PRICER
 #define _PRICER
 
-#include <vector>
 #include <cmath>
+#include <vector>
 
+#include "Market.h"
 #include "Trade.h"
 #include "TreeProduct.h"
-#include "Market.h"
 
-//interface
+// interface
 class Pricer {
-public:
-  // virtual double Price(const Market& mkt, std::unique_ptr<Trade>& trade);
-  virtual double Price(const Market& mkt, Trade* trade);
-  virtual double CalculateDV01(const Market& market, Trade* trade);
-  virtual double CalculateVega(const Market& market, Trade* trade);
+  public:
+    // virtual double Price(const Market& mkt, std::unique_ptr<Trade>& trade);
+    virtual double Price(const Market &mkt, Trade *trade,
+                         const Date &valueDate);
+    virtual double CalculateDV01(const Market &market, Trade *trade,
+                                 const Date &valueDate);
+    virtual double CalculateVega(const Market &market, Trade *trade,
+                                 const Date &valueDate);
 
-private:
-  virtual double PriceTree(const Market& mkt, const TreeProduct& trade) { return 0; };
+  private:
+    virtual double PriceTree(const Market &mkt, const TreeProduct &trade) {
+        return 0;
+    };
 };
 
+class BinomialTreePricer : public Pricer {
+  public:
+    BinomialTreePricer(int N) {
+        nTimeSteps = N;
+        states.resize(N + 1);
+    }
+    double PriceTree(const Market &mkt, const TreeProduct &trade) override;
 
+  protected:
+    virtual void ModelSetup(double S0, double sigma, double rate,
+                            double dt) = 0; // pure virtual
+    virtual double GetSpot(int ti, int si) const = 0;
+    virtual double GetProbUp() const = 0;
+    virtual double GetProbDown() const = 0;
 
-class BinomialTreePricer: public Pricer
-{
-public:
-  BinomialTreePricer(int N) {
-    nTimeSteps = N;
-    states.resize(N+1);
-  }
-  double PriceTree(const Market& mkt, const TreeProduct& trade) override;
-  
-protected:
-  virtual void ModelSetup(double S0, double sigma, double rate, double dt) = 0; // pure virtual
-  virtual double GetSpot(int ti, int si) const = 0;
-  virtual double GetProbUp() const = 0;
-  virtual double GetProbDown() const = 0;
-  
-private:
-  int nTimeSteps;
-  std::vector<double> states;
+  private:
+    int nTimeSteps;
+    std::vector<double> states;
 };
 
-class CRRBinomialTreePricer : public BinomialTreePricer
-{
-public:
-  CRRBinomialTreePricer(int N) : BinomialTreePricer(N){}
+class CRRBinomialTreePricer : public BinomialTreePricer {
+  public:
+    CRRBinomialTreePricer(int N) : BinomialTreePricer(N) {}
 
-protected:
-  void ModelSetup(double S0, double sigma, double rate, double dt); 
-  double GetSpot(int ti, int si) const {
-    return currentSpot * std::pow(u, ti-2*si); 
-  }
-  double GetProbUp() const {return p;}
-  double GetProbDown() const {return 1-p;}
-  
-private:
-  double u; // up multiplicative
-  double p; // probability for up state
-  double currentSpot; // current market spot price
+  protected:
+    void ModelSetup(double S0, double sigma, double rate, double dt);
+    double GetSpot(int ti, int si) const {
+        return currentSpot * std::pow(u, ti - 2 * si);
+    }
+    double GetProbUp() const { return p; }
+    double GetProbDown() const { return 1 - p; }
+
+  private:
+    double u;           // up multiplicative
+    double p;           // probability for up state
+    double currentSpot; // current market spot price
 };
 
-class JRRNBinomialTreePricer : public BinomialTreePricer
-{
-public:
-  JRRNBinomialTreePricer(int N) : BinomialTreePricer(N){}
- 
-protected:
-  void ModelSetup(double S0, double sigma, double rate, double dt);
-  
-  double GetSpot(int ti, int si) const
-  {
-    return currentSpot * std::pow(u, ti-si) * std::pow(d, si);
-  }
-  double GetProbUp() const {return p;}
-  double GetProbDown() const {return 1-p;}
+class JRRNBinomialTreePricer : public BinomialTreePricer {
+  public:
+    JRRNBinomialTreePricer(int N) : BinomialTreePricer(N) {}
 
-private:
-  double u; // up multiplicative
-  double d; // down multiplicative
-  double p; // probability for up state
-  double currentSpot; // current market spot price
-}; 
+  protected:
+    void ModelSetup(double S0, double sigma, double rate, double dt);
+
+    double GetSpot(int ti, int si) const {
+        return currentSpot * std::pow(u, ti - si) * std::pow(d, si);
+    }
+    double GetProbUp() const { return p; }
+    double GetProbDown() const { return 1 - p; }
+
+  private:
+    double u;           // up multiplicative
+    double d;           // down multiplicative
+    double p;           // probability for up state
+    double currentSpot; // current market spot price
+};
 
 #endif
