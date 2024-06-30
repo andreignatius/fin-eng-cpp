f3b236db843c5c41ee778228ef39e8ca0de20b88
Joseph Adhika <jt.adhika.2023@mqf.smu.edu.sg>
2024-07-01 02:39:09 +0800

    payoff with custom curve for DV01

--- Bond.cpp
+++ Bond.cpp
@@ -1,7 +1,7 @@
 #include "Bond.h"
 #include <sstream>
 
-double Bond::Payoff(double marketPrice) const {
+double Bond::PayoffCurve(RateCurve theCurve) const {
     double couponAmount; // Use internal market data
     double currentRate = 0.0;
     double rate;
@@ -17,8 +17,11 @@ double Bond::Payoff(double marketPrice) const {
     vector<Date> paymentSchedule;
     // 1. get coupon amount
     couponAmount = notional * couponRate * frequency;
-
-    // 2. generate the bond schedule; CANNOT USE FUNCTION BECAUSE const keyword
+    // display the curve
+    // std::cout << "CONSTRUCT BOND WITH CUSTOM CURVE" << std::endl;
+    // theCurve.display();
+    // 2. generate the bond schedule; CANNOT USE FUNCTION BECAUSE const
+    // keyword
     /*
         PAYMENT SCHEDULE GENERATION START
     */
@@ -50,8 +53,88 @@ double Bond::Payoff(double marketPrice) const {
     for (int i = 0; i < numPeriods; ++i) {
         if (paymentSchedule[i] <= valueDate) {
             // coupon already disbursed
-            std::cout << paymentSchedule[i] << " < " << valueDate
-                      << " ; NOT INCLUDED IN BOND PV CALC" << std::endl;
+        } else {
+            // the rest
+            yearsSinceStart =
+                static_cast<double>(
+                    paymentSchedule[i].differenceInDays(valueDate)) /
+                Constants::NUM_DAYS_IN_YEAR; // Convert days to years
+            rate = theCurve.getRate(paymentSchedule[i]);
+            double discountFactor = exp(-rate * yearsSinceStart);
+            if (i == paymentCount - 1) {
+                // on maturity coupon + notional
+                pv += discountFactor * (couponAmount + notional);
+                /*
+                std::cout << "COUNT FINAL " << i
+                          << " DATE : " << paymentSchedule[i] << " PV : " <<
+                pv
+                          << std::endl;
+                          */
+            } else {
+                // only coupon
+                pv += discountFactor * couponAmount;
+                /*
+                std::cout << "COUNT " << i << " DATE : " <<
+                paymentSchedule[i]
+                          << " PV : " << pv << std::endl;
+                          */
+            }
+        }
+    }
+    std::cout << "FINAL PV = " << pv << std::endl;
+    return pv;
+}
+
+double Bond::Payoff(double marketPrice) const {
+    double couponAmount; // Use internal market data
+    double currentRate = 0.0;
+    double rate;
+    double pv = 0;
+    double yearsSinceStart;
+    double fixedLegPV;
+    double floatLegPV;
+    double DF_last;
+    int paymentInMonths;
+    int paymentCount;
+    Date paymentDate = startDate;
+    Date seed = maturityDate;
+    vector<Date> paymentSchedule;
+    // 1. get coupon amount
+    couponAmount = notional * couponRate * frequency;
+
+    // 2. generate the bond schedule; CANNOT USE FUNCTION BECAUSE const
+    // keyword
+    /*
+        PAYMENT SCHEDULE GENERATION START
+    */
+    paymentInMonths = static_cast<int>(12 * frequency);
+    while (seed > startDate) {
+        paymentSchedule.push_back(seed);
+        seed.subtractMonths(paymentInMonths);
+    }
+    paymentSchedule.push_back(startDate);
+    if (paymentSchedule.size() < 2)
+        throw std::runtime_error("Error: invalid schedule, check input!");
+    std::reverse(paymentSchedule.begin(), paymentSchedule.end());
+    /*
+        PAYMENT SCHEDULE GENERATION END
+    */
+
+    // 3. now do the calculation
+
+    // 4. Follow Swap a bit for PV calc
+    long daysBetween = maturityDate.differenceInDays(startDate);
+    double yearsBetween = static_cast<double>(daysBetween) /
+                          Constants::NUM_DAYS_IN_YEAR; // Convert days to years
+    int numPeriods = paymentSchedule.size();
+    std::cout << "numPeriods : " << numPeriods << " ; startDate : " << startDate
+              << " ; maturityDate : " << maturityDate
+              << " ; Notional : " << notional
+              << " ; Coupon Amt : " << couponAmount << " ; Freq : " << frequency
+              << std::endl;
+    for (int i = 0; i < numPeriods; ++i) {
+        if (paymentSchedule[i] <= valueDate) {
+            // coupon already disbursed
         } else {
             // the rest
             yearsSinceStart =
@@ -66,14 +149,16 @@ double Bond::Payoff(double marketPrice) const {
                 pv += discountFactor * (couponAmount + notional);
                 /*
                 std::cout << "COUNT FINAL " << i
-                          << " DATE : " << paymentSchedule[i] << " PV : " << pv
+                          << " DATE : " << paymentSchedule[i] << " PV : " <<
+                pv
                           << std::endl;
                           */
             } else {
                 // only coupon
                 pv += discountFactor * couponAmount;
                 /*
-                std::cout << "COUNT " << i << " DATE : " << paymentSchedule[i]
+                std::cout << "COUNT " << i << " DATE : " <<
+                paymentSchedule[i]
                           << " PV : " << pv << std::endl;
                           */
             }
